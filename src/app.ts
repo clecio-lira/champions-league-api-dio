f8a0c9437f3d0918f52ea88d617ad07459116f37
Clécio de Lira Alves <cclecioc.com@gmail.com>
2025-07-26 10:03:52 -0300

    feat: starting

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
+import cors from "cors";
 import express, { json } from "express";
 import { router } from "./routes";
 
 export default function createApp() {
   const app = express();
 
+  app.use(cors());
   app.use(json());
   app.use("/api", router);
 
